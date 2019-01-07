403e40cd92a62f2b14f4c16c1f7b0a50aa6c8243
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2019-01-07 00:00:19 -0600

    started the work

---
+++ Api/Startup.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Dal;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using static Api.Utilities.ConnectionStringUtility;
+using HostingEnvironmentExtension = Api.Extensions.HostingEnvironmentExtension;
+
+namespace Api
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+        
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(builder =>
+            {
+                if (HostingEnvironmentExtension.IsLocalhost(_env))
+                {
+                    builder.UseSqlite(_configuration.GetValue<string>("ConnectionStrings:Sqlite"));    
+                }
+                else
+                {
+                    builder.UseNpgsql(ConnectionStringUrlToResource(Environment.GetEnvironmentVariable("DATABASE_URL"))
+                                      ?? throw new Exception("DATABASE_URL is null"));
+                }
+            });
+            
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Dal/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal
+{
+    public sealed class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
